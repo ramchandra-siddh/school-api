855bf0d53f0fd28850b8d20e48ebbbda080b9ba1
ramchandra-siddh <ramchandrasiddh18@gmail.com>
2025-06-19 10:08:47 +0530

    fix: use dynamic port for Railway

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get("/listSchools", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server started on http://localhost:${PORT}`);
