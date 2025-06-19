65f467ed53b8df60884a52b1d8c71741369d2ff1
ramchandra-siddh <ramchandrasiddh18@gmail.com>
2025-06-19 10:00:29 +0530

    fix: use dynamic port for Railway

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).send("API is running");
+});
+
 //POST  /addSchool
 
 app.post("/addSchool", (req, res) => {
